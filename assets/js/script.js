1ef901a413942d9106d75d5ab737bce0f0323467
k-frederic <>
2023-10-06 03:37:27 +0000

    Set up structure of game function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,31 @@
- 
+ function runGame() {
+    
+ }
+
+  function checkAnswer() {
+    
+ }
+
+  function calculateCorrectAnswer() {
+    
+ }
+
+  function incrementScore() {
+    
+ }
+
+  function incrementWrongAnswer() {
+    
+ }
+
+  function displayAdditionQuestion() {
+    
+ }
+
+function displaySubstractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
