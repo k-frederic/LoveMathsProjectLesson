a39d89a1036e535706e56adf942833e22559030d
k-frederic <>
2023-10-10 20:43:36 +0000

    Add multiplication game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,6 +25,8 @@ function runGame(gameType) {
 
     if (gameType === 'addition') {
         displayAdditionQuestion(num1, num2);
+    } else if (gameType === 'multiply') {
+        displayMultiplyQuestion(num1, num2);
     }
     else {
         alert(`Unknown game type: ${gameType}`);
@@ -50,7 +52,7 @@ function checkAnswer() {
     alert (`Awwww.... You answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
     incrementWrongAnswer();
    }
-   
+
   runGame(calculatedAnswer[1]);
 }
 
@@ -65,6 +67,8 @@ function calculateCorrectAnswer() {
 
     if (operator === "+") {
         return[operand1 + operand2, 'addiction'];
+    } else if (operator === "x") {
+        return [operand1 * operand2, "multiply" ];
     }
     else {
         alert(`<Unimplemented operator ${operator}`);
@@ -100,6 +104,8 @@ function displaySubstractQuestion() {
 
 }
 
-function displayMultiplyQuestion() {
-
+function displayMultiplyQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
 }
